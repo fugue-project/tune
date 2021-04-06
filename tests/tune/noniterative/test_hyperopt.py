a5f359ab0cf9aa670be04f4445595e73ae22cbd2
Han Wang <goodwanghan@gmail.com>
2021-04-06 17:52:13 +0000

    update

--- tests/tune/noniterative/test_hyperopt.py
+++ tests/tune/noniterative/test_hyperopt.py
@@ -9,14 +9,14 @@ from tune.trial import Trial
 def test_hyperopt():
     params = dict(a=Rand(-10.0, 10.0), b=RandInt(-100, 100), c=2.0)
     trial = Trial("a", params, metadata={})
-    h = HyperoptRunner(max_iter=300, seed=0)
+    h = HyperoptRunner(max_iter=200, seed=0)
 
     @noniterative_objective
     def objective(a, b, c) -> Tuple[float, Dict[str, Any]]:
         return a ** 2 + b ** 2 + c, dict(a=1)
 
     report = h.run(objective, trial)
-    assert report.metric < 3
-    assert report.params["a"] ** 2 < 1
-    assert report.params["b"] ** 2 < 1
+    assert report.metric < 7
+    assert report.params["a"] ** 2 < 2
+    assert report.params["b"] ** 2 < 2
     assert 2.0 == report.params["c"]
