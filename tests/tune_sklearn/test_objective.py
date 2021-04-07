c0623bef9073de83585efb0e78a77cfcfb25ae55
Han Wang <goodwanghan@gmail.com>
2021-04-07 06:32:25 +0000

    update

--- tests/tune_sklearn/test_objective.py
+++ tests/tune_sklearn/test_objective.py
@@ -1,10 +1,11 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from tune import RandInt, Trial
+from tune.constants import TUNE_DATASET_DF_DEFAULT_NAME
 from tune.noniterative.hyperopt import HyperoptRunner
 from tune.noniterative.objective import validate_noniterative_objective
 
-from tune_sklearn.constants import TUNE_DATASET_DF_DEFAULT_NAME, MODEL_PARAM_NAME
+from tune_sklearn.constants import MODEL_PARAM_NAME
 from tune_sklearn.objective import SKCVObjective
 
 
@@ -34,7 +35,7 @@ def test_objective(tmpdir):
     validate_noniterative_objective(obj, t, v, runner=runner)
 
     obj = SKCVObjective(scoring="accuracy", checkpoint_path=str(tmpdir))
-    
+
     def v2(report):
         print(report.jsondict)
         assert report.sort_metric < 0
