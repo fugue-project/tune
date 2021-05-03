fa3edaf983734e9d01ace5d65fc3c2c7a2a58b13
Han Wang <goodwanghan@gmail.com>
2021-05-03 18:05:18 +0000

    update

--- tune_notebook/monitors.py
+++ tune_notebook/monitors.py
@@ -24,22 +24,30 @@ class PrintBest(Monitor):
 
 
 class NotebookSimpleChart(Monitor):
-    def __init__(self, interval: Any = "1sec"):
+    def __init__(
+        self,
+        interval: Any = "1sec",
+        best_only: bool = True,
+        always_update: bool = False,
+    ):
         super().__init__()
         self._lock = RLock()
         self._last: Any = None
         self._bins: Dict[str, "_ReportBin"] = {}
         self._interval = to_timedelta(interval)
+        self._best_only = best_only
+        self._always_update = always_update
 
     def on_report(self, report: TrialReport) -> None:
         now = datetime.now()
         with self._lock:
             key = str(report.trial.keys)
             if key not in self._bins:
-                self._bins[key] = _ReportBin(new_best_only=True)
+                self._bins[key] = _ReportBin(new_best_only=self._best_only)
             rbin = self._bins[key]
 
-        if not rbin.on_report(report):
+        updated = rbin.on_report(report)
+        if not updated and not self._always_update:
             return
 
         with self._lock:
@@ -83,7 +91,7 @@ class NotebookSimpleChart(Monitor):
 
 class NotebookSimpleRungs(NotebookSimpleChart):
     def __init__(self, interval: Any = "1sec"):
-        super().__init__(interval)
+        super().__init__(interval, best_only=False, always_update=True)
 
     def plot(self, df: pd.DataFrame) -> None:
         import seaborn as sns
