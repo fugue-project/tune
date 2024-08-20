bceef075262b54b1987e15e8c7ebd19e5fae5df1
Han Wang <goodwanghan@gmail.com>
2024-08-20 06:40:27 +0000

    update

--- tests/tune/concepts/test_logger.py
+++ tests/tune/concepts/test_logger.py
@@ -9,8 +9,10 @@ def test_logger_context():
     m1 = MetricLogger()
     m2 = MetricLogger()
 
-    with set_current_metric_logger(m1):
-        assert get_current_metric_logger() == m1
-        with set_current_metric_logger(m2):
-            assert get_current_metric_logger() == m2
-        assert get_current_metric_logger() == m1
+    with set_current_metric_logger(m1) as mm1:
+        assert get_current_metric_logger() is m1
+        assert mm1 is m1
+        with set_current_metric_logger(m2) as mm2:
+            assert get_current_metric_logger() is m2
+            assert mm2 is m2
+        assert get_current_metric_logger() is m1
